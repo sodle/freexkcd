82a5b4cce62c202dfec5f53f709d393b89ca4212
Scott Odle <scott@sjodle.com>
2022-09-30 10:45:39 -0700

    Initial Commit

---
+++ freexkcd/freexkcdApp.swift
@@ -0,0 +1,17 @@
+//
+//  freexkcdApp.swift
+//  freexkcd
+//
+//  Created by Scott Odle on 9/30/22.
+//
+
+import SwiftUI
+
+@main
+struct freexkcdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
